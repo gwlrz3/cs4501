8434f43fe4ffe5efdfec98a07a7625ed7685038e
gwlrz3 <chenlt0522@gmail.com>
2018-10-19 10:48:49 +0800

    test

--- 4-tier-app/model_layer/mymodel/modelapp/tests.py
+++ 4-tier-app/model_layer/mymodel/modelapp/tests.py
@@ -1,14 +1,8 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
-class ListStudentTestCase(TestCase):
-    def setUp(self):
-        pass #nothing to set up
+class ListStudentTests(TestCase):
 
      def success_response(self):
-        response = self.client.get('/hall/list')
+        response = self.client.get('/student/list')
         self.assertEquals(response.status_code, 200)
-
-    #tearDown method is called after each test
-    def tearDown(self):
-        pass #nothing to tear down
