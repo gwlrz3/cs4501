ea4c1c4edf45ee3a47430191f02c2fbaf3450fe5
fxchang23 <chang7156@gmail.com>
2018-10-18 23:02:55 -0400

    create unit test

--- 4-tier-app/model_layer/mymodel/modelapp/tests.py
+++ 4-tier-app/model_layer/mymodel/modelapp/tests.py
@@ -1,14 +1,38 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
-class ListStudentTestCase(TestCase):
-    def setUp(self):
-        pass #nothing to set up
 
-     def success_response(self):
-        response = self.client.get('/hall/list')
+class ListStudentTests(TestCase):
+    def test_success_response(self):
+        response = self.client.get('/modelapp/student/list')
         self.assertEquals(response.status_code, 200)
 
-    #tearDown method is called after each test
-    def tearDown(self):
-        pass #nothing to tear down
+
+class ListHallTests(TestCase):
+    def test_success_response(self):
+        response = self.client.get('/modelapp/hall/list')
+        self.assertEquals(response.status_code, 200)
+
+
+class ListManagerTests(TestCase):
+    def test_success_response(self):
+        response = self.client.get('/modelapp/manager/list')
+        self.assertEquals(response.status_code, 200)
+
+
+class ListAdvisorTests(TestCase):
+    def test_success_response(self):
+        response = self.client.get('/modelapp/advisor/list')
+        self.assertEquals(response.status_code, 200)
+
+
+class ListRoomTests(TestCase):
+    def test_success_response(self):
+        response = self.client.get('/modelapp/room/list')
+        self.assertEquals(response.status_code, 200)
+
+
+class ListLeaseTests(TestCase):
+    def test_success_response(self):
+        response = self.client.get('/modelapp/lease/list')
+        self.assertEquals(response.status_code, 200)
