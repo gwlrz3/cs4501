e618b8191efe8b4ba20e7229587395df4978508c
gwlrz3 <chenlt0522@gmail.com>
2018-10-19 10:59:15 +0800

    test

--- 4-tier-app/model_layer/mymodel/modelapp/tests.py
+++ 4-tier-app/model_layer/mymodel/modelapp/tests.py
@@ -6,3 +6,39 @@ class ListStudentTests(TestCase):
      def test_success_response(self):
         response = self.client.get('/student/list')
         self.assertEquals(response.status_code, 200)
+
+class ListStudentTests(TestCase):
+
+     def test_success_response(self):
+        response = self.client.get('/student/list')
+        self.assertEquals(response.status_code, 200)
+
+class ListAdvisorTests(TestCase):
+
+     def test_success_response(self):
+        response = self.client.get('/advisor/list')
+        self.assertEquals(response.status_code, 200)
+
+class ListManagerTests(TestCase):
+
+     def test_success_response(self):
+        response = self.client.get('/manager/list')
+        self.assertEquals(response.status_code, 200)
+
+class ListHallTests(TestCase):
+
+     def test_success_response(self):
+        response = self.client.get('/hall/list')
+        self.assertEquals(response.status_code, 200)
+
+class ListRoomTests(TestCase):
+
+     def test_success_response(self):
+        response = self.client.get('/room/list')
+        self.assertEquals(response.status_code, 200)
+
+class ListLeaseTests(TestCase):
+
+     def test_success_response(self):
+        response = self.client.get('/lease/list')
+        self.assertEquals(response.status_code, 200)
