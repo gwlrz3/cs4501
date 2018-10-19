14f9178adf41e550946aab3b7f145c220b26b7f3
gwlrz3 <chenlt0522@gmail.com>
2018-10-19 10:51:05 +0800

    test

--- 4-tier-app/model_layer/mymodel/modelapp/tests.py
+++ 4-tier-app/model_layer/mymodel/modelapp/tests.py
@@ -3,6 +3,6 @@ from django.urls import reverse
 
 class ListStudentTests(TestCase):
 
-     def success_response(self):
+     def test_success_response(self):
         response = self.client.get('/student/list')
         self.assertEquals(response.status_code, 200)
