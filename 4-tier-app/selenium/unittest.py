3bb480308c2fd819f12c8cf2a6091d8bfb5589b6
fxchang23 <chang7156@gmail.com>
2018-12-18 21:29:33 -0500

    fix conflict

--- 4-tier-app/selenium/unittest.py
+++ 4-tier-app/selenium/unittest.py
@@ -1,10 +1,13 @@
 # import unittest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
+
+time.sleep(10)
 
 driver = webdriver.Remote(
     command_executor='http://selenium-chrome:4444/wd/hub',
-    desired_capabilities=DesiredCapabilities.CHROME)
+    desired_capabilities=DesiredCapabilities().CHROME)
 
 driver.get("http://web:8000")
 assert "Office" in driver.title
